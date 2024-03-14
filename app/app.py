440a7ad85e3f461f3bc4ae60f8fd0abd273fd5e1
Luis García <pruebasdaw2021@gmail.com>
2024-03-14 14:29:43 +0100

    Iteracion lista de los cursos en el index

--- app/app.py
+++ app/app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return "<h1>Hola mundo desde Flask en 2024 que tal?</h1>"
+    cursos = ['PHP','Python','Java','Javascript','C#','C++', 'Go', 'Kotlin']
+    data = {
+        'titulo': 'Index',
+        'bienvenida': 'Saludos!!!',
+        'cursos': cursos,
+        'numero_cursos': len(cursos)
+    }
 
-    return render_template('index.html')
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
