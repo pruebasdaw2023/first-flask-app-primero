a09f8d75d08e00b39eceb73a06a5f7295a74da59
Luis García <pruebasdaw2021@gmail.com>
2024-03-14 14:03:39 +0100

    Añadido el primer template del index.html

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hola mundo desde Flask en 2024 que tal?</h1>"
+    #return "<h1>Hola mundo desde Flask en 2024 que tal?</h1>"
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
